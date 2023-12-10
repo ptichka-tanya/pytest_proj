e70245f7ac2fe8d18958e9f80854b7f94b26304e
ptichka-tanya <t.n.sinitsina@gmail.com>
2023-12-10 15:49:51 +0300

    add tests to cover 100%

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -5,6 +5,7 @@ def test_get():
     assert arrs.get([1, 2, 3], 1, "test") == 2
     assert arrs.get([1, 5, "word", 3, "some text"], 2, "test") == "word"
     assert arrs.get([], 0, "test") == "test"
+    assert arrs.get([57, 45, 84, "cat", "dog"], 7, "test") == "test"
 
 
 def test_my_slice():
@@ -14,4 +15,5 @@ def test_my_slice():
     assert arrs.my_slice([41, 27, 3, 17, "frog", 8, "some text", True, 56], 4, None) == ["frog", 8, "some text", True, 56]
     assert arrs.my_slice([7, 62, 5, 4, 15, 74], 1, 5) == [62, 5, 4, 15]
     assert arrs.my_slice([1, 2, 3, 4], None, 5) == [1, 2, 3, 4]
+    assert arrs.my_slice([], None, 5) == []
 
